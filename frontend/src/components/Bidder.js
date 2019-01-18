c9e5800a962677ff6ad88950b16a69f36123b7c3
Nate Poupard <poupardn@gmail.com>
2019-01-18 14:58:21 -0500

    upd

--- frontend/src/components/Bidder.js
+++ frontend/src/components/Bidder.js
@@ -4,23 +4,39 @@ import { TextField } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 
 export default class Bidder extends React.Component {
-    state = {
-        bidderName: '',
+
+    constructor() {
+        super();
+        this.state = { bidderName: null };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            bidderName: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        sessionStorage.setItem('bidderName', this.state.bidderName);
+        this.forceUpdate();
     }
 
     render() {
         return (
             <div>
-                {this.state.bidderName === '' ? (
+                {sessionStorage.getItem('bidderName') === null ? (
                     <form onSubmit={this.handleSubmit}>
                         <TextField style={{ padding: 24 }}
                             id="bidderNameInput"
                             placeholder="Enter Bidder Name to Bid..."
-                            margin="normal" />
+                            margin="normal"
+                            onChange={this.handleChange} />
 
-
+                        <Button type="submit" variant="flat" size="small" color="primary">Register Bidder</Button>
                     </form>
-                ) : null}
+                ) : this.state.bidderName}
+
             </div>
         )
     }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import { AppBar } from '@material-ui/core';
 import { Toolbar } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
-const NavBar = () => {
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    }
+}
+
+const NavBar = (props) => {
+    const { classes } = props;
     return (
-        <div>
+        <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="title" color="inherit">
-                        SuperAuctions Auction Application
+                    <Typography variant="H6" color="inherit" className={classes.grow}>
+                        SuperAuction Auctions
                     </Typography>
+                    <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
         </div>
     );
 }
 
-export default NavBar;
+NavBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NavBar);
